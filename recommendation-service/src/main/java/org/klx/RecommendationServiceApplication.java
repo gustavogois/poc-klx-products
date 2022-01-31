4597168f45b0a09e4dee984f9fdec3c8b612e6dc
Gustavo Gois <gustavo.gois@devoteam.com>
2022-01-31 15:44:33 +0000

    RecommendationService API

---
+++ recommendation-service/src/main/java/org/klx/RecommendationServiceApplication.java
@@ -0,0 +1,14 @@
+package org.klx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan("org.klx")
+public class RecommendationServiceApplication
+{
+    public static void main( String[] args ) {
+        SpringApplication.run(RecommendationServiceApplication.class, args);
+    }
+}
